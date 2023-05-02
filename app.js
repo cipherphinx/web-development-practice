55a8303da130cf7a2a25d9c5f79c85f1760818b8
cipherphinx <cipherphinx@gmail.com>
2023-05-02 14:49:59 +0800

    commiting to remote git repo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const _ = require("lodash");
+const ejs = require("ejs");
 
 const app = express();
 
@@ -14,138 +14,117 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-phinx:test123@cluster0-1ikzn.mongodb.net/todolistDB", {
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
   useNewUrlParser: true
 });
 
-const itemsSchema = {
-  name: String
+const articleSchema = {
+  title: String,
+  content: String
 };
 
-const Item = mongoose.model("Item", itemsSchema);
+const Article = mongoose.model("Article", articleSchema);
 
-const item1 = new Item({
-  name: "Welcome to your todolist!"
-});
+//////////////////////////////////Requests Targeting all Articles//////////////////////////
 
-const item2 = new Item({
-  name: "Hit the + button to add a new item."
-});
+app.route("/articles")
 
-const item3 = new Item({
-  name: "<-- Hit this to delete an item."
-});
-
-const defaultItems = [item1, item2, item3];
-
-const listSchema = {
-  name: String,
-  items: [itemsSchema]
-};
-
-const List = mongoose.model("List", listSchema);
-
-app.get("/", function(req, res) {
-
-  Item.find({}, function(err, foundItems) {
-
-    if (foundItems.length === 0) {
-      Item.insertMany(defaultItems, function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Succefully saved the default item in DB.");
-        };
-      });
-      res.redirect("/");
-    } else {
-      res.render("list", {
-        listTitle: "Today",
-        newListItems: foundItems
-      });
-    };
-  });
-
-});
-
-app.post("/", function(req, res) {
-
-  const itemName = req.body.newItem;
-  const listName = req.body.list;
-
-  const item = new Item({
-    name: itemName
-  });
-
-  if (listName === "Today") {
-    item.save();
-    res.redirect("/");
-  } else {
-    List.findOne({name: listName}, function(err, foundList){
-      foundList.items.push(item);
-      foundList.save();
-      res.redirect("/" + listName);
-    });
-  };
-});
-
-app.post("/delete", function(req, res) {
-  const checkedItemId = req.body.checkbox;
-  const listName = req.body.listName;
-
-  if (listName === "Today") {
-    Item.findByIdAndRemove(checkedItemId, function(err) {
-      if (!err) {
-        console.log("Succesfully deleted check item.");
-        res.redirect("/");
-      };
-    });
-  } else {
-    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
-      if (!err) {
-        res.redirect("/" + listName);
-      }
-    });
-  }
-
-
-
-});
-
-app.get("/:customListName", function(req, res) {
-  const customListName = _.capitalize(req.params.customListName);
-
-  List.findOne({name: customListName}, function(err, foundList) {
+.get(function(req, res){
+  Article.find({}, function(err, foundArticles){
     if (!err) {
-      if (!foundList) {
-        //Create a new list
-        const list = new List({
-          name: customListName,
-          items: defaultItems
-        });
-        list.save();
-        res.redirect("/" + customListName);
-      } else {
-        //Show an existing list
-        res.render("list", {
-          listTitle: foundList.name,
-          newListItems: foundList.items
-        });
-      };
-    };
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post(function(req, res){
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
   });
 
+  newArticle.save(function(err){
+    if (!err) {
+      res.send("Successfully added new article.")
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function(req, res){
+  Article.deleteMany(function(err){
+    if (!err) {
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
 });
 
-app.get("/about", function(req, res) {
-  res.render("about");
+//////////////////////////////////Requests Targeting A Specific Article//////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
+    }
+
+  });
+})
+
+.put(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if (!err) {
+        res.send("Successfully updated article.");
+      }
+    }
+  );
+})
+
+.patch(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if (!err) {
+        res.send("Successfully updated article.")
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+    if (!err) {
+      res.send("Successfully deleted the corresponding article.");
+    } else {
+      res.send(err);
+    }
+  });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
-app.listen(port, function() {
-  console.log("Server has started successfully.");
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
