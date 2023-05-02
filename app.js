2fca3f6a3952f5237f9255082e32f3f3f3dfef9f
cipherphinx <cipherphinx@gmail.com>
2023-05-02 09:49:21 +0800

    commiting to remote git repo

--- app.js
+++ app.js
@@ -1,70 +1,121 @@
 //jshint esversion:6
+const mongoose = require("mongoose");
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash");
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-let posts = [];
-
-app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-  });
+const fruitSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: [true, "Please check your data entry, no name specified!"]
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  review: String
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {
-    aboutContent: aboutContent
-  });
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+  rating: 10,
+  review: "Peaches are so yummy."
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {
-    contactContent: contactContent
-  });
+// fruit.save();
+
+const personSchema = new mongoose.Schema ({
+  name: String,
+  age: Number,
+  favouriteFruit: fruitSchema
 });
 
-app.get("/compose", function(req, res){
-  res.render("compose");
+const Person = mongoose.model("Person", personSchema);
+
+const grape = new Fruit({
+  name: "Grape",
+  rating: 9.5,
+  review: "Sweet and tangy."
 });
 
-app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
+grape.save();
+
+Person.updateOne({_id: "5d1de9f6434d7a2e4cb7e97f"}, {favouriteFruit: grape}, function(err){
+  if (err){
+    console.log(err);
+  } else {
+    console.log("Successfully updated the document.");
   };
-  posts.push(post);
-  res.redirect("/");
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+// const person = new Person ({
+//   name: "Amy",
+//   age: 12,
+//   favouriteFruit: pineapple
+// });
+//
+// person.save();
 
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
+Fruit.find(function(err, fruits){
+  if (err) {
+    console.log(err);
+  } else {
 
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    };
-  });
+    mongoose.connection.close();
+
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    });
+  };
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+// Fruit.updateOne({_id: "5d1de24e12fb683878d157a3"}, {name: "Peach"}, function(err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("Successfully updated the document.");
+//   };
+// });
+
+// Fruit.deleteOne({name: "Peach"}, function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully deleted the record!");
+//   };
+// });
+
+// Person.deleteMany({name: "John"}, function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully deleted all the record!");
+//   }
+// })
+
+// const kiwi = new Fruit({
+//   name: "Kiwi",
+//   rating: 10,
+//   review: "The best fruit!"
+// });
+//
+// const orange = new Fruit({
+//   name: "Orange",
+//   rating: 4,
+//   review: "Too sour for me"
+// });
+//
+// const banana = new Fruit({
+//   name: "Banana",
+//   rating: 3,
+//   review: "Weird texture"
+// });
+//
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfuly saved all the fruits.");
+//   };
+// });
